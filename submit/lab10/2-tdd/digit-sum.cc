946716146675cc2662c5ee847920275962625ec5
Brendan Klayman <bklayma1@binghamton.edu>
2020-04-16 16:43:50 -0400

    Submitted lab 10

---
+++ submit/lab10/2-tdd/digit-sum.cc
@@ -0,0 +1,30 @@
+#include <cctype>
+#include <iostream>
+
+#include "digit-sum.hh"
+
+int
+digitSum(const char* str)
+{
+  bool wantDigit = true;
+  int acc = 0;
+  for(const char* p = str; *p; ++p){
+    int c = *p;
+    if(isdigit(c)){
+      if(wantDigit){
+        acc+= *p - '0';
+        wantDigit = false;
+      } else {
+        break;
+      }
+    } else if (c == '+') {
+      if(wantDigit){
+        break;
+      }
+      wantDigit = true;
+    } else if (c != ' '){
+      break;
+    }
+  }
+  return acc;
+}
